30b7adfda986a70f12bb9a90b64f00b3e5f729be
Arve Hjønnevåg <arve@android.com>
2017-04-06 19:40:01 -0700

    [lib][io] Add WITHOUT_CONSOLE option.
    Allows compiling a 32 bit user-space libc when using a 64 bit kernel.
    
    Change-Id: I91c72408fecef5be6203d0e079169e040ea2ab30

--- lib/io/rules.mk
+++ lib/io/rules.mk
@@ -3,10 +3,13 @@ LOCAL_DIR := $(GET_LOCAL_DIR)
 MODULE := $(LOCAL_DIR)
 
 MODULE_DEPS := \
-	lib/cbuf
+
+ifeq (false,$(call TOBOOL,$(WITHOUT_CONSOLE)))
+MODULE_DEPS += lib/cbuf
+MODULE_SRCS += $(LOCAL_DIR)/console.c
+endif
 
 MODULE_SRCS += \
-   $(LOCAL_DIR)/console.c \
    $(LOCAL_DIR)/io.c \
 
 include make/module.mk
