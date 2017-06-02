093b1337810236d2677d3b45eabd2af692162975
Zhang, Qi <qi1.zhang@intel.com>
2017-06-02 10:01:12 +0800

    [lib][libc] memcpy and memset implementation
    Change-Id: I87d932a5dd6edd811bea42200f5c67509748c43e
    Signed-off-by: Zhang, Qi <qi1.zhang@intel.com>

--- lib/libc/string/arch/x86/64/rules.mk
+++ lib/libc/string/arch/x86/64/rules.mk
@@ -3,8 +3,8 @@ LOCAL_DIR := $(GET_LOCAL_DIR)
 ASM_STRING_OPS := memcpy memset
 
 MODULE_SRCS += \
-	$(LOCAL_DIR)/memcpy.c \
-	$(LOCAL_DIR)/memset.c
+	$(LOCAL_DIR)/memcpy.S \
+	$(LOCAL_DIR)/memset.S
 
 # filter out the C implementation
 C_STRING_OPS := $(filter-out $(ASM_STRING_OPS),$(C_STRING_OPS))
